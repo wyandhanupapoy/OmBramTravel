16d670becf5cf6632d1998e1ad89b05ab1dc9406
Wyandhanu Maulidan Nugraha <wyandhanupapoy@gmail.com>
2026-08-22 00:38:58 +0700

    chore: add google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import { ReactNode } from "react";
+import { Metadata } from "next";
 import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "uM3l19nlPDO4zX2XXqJFP_Xg4Q0ZFlEULjRr15K0YlI",
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
